b7b5c93f8d460945cfbe9cbfc5e95170d01bbaee
Zain Shaikh <skzain007995@gmail.com>
2026-07-20 19:15:43 +0530

    Add delete functionality for notes in the API

--- Express/src/app.js
+++ Express/src/app.js
@@ -19,10 +19,20 @@ app.post('/notes', (req, res) =>{
 })
 
 app.get('/notes', (req, res) =>{
-    
+
     res.status(200).json({
         message : "Notes fetched successfully",
         notes : notes
+    })
+})
+
+app.delete('/notes/:index', (req, res)=>{
+    const index = req.params.index
+
+    delete notes[index]
+
+    res.status(200).json({
+        message: "Note deleted successfully"
     })
 })
 
