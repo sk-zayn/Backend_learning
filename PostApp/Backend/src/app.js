239a7ee723b2f31ecb235a200b3bab0c933af73a
Zain Shaikh <skzain007995@gmail.com>
2026-07-23 20:35:09 +0530

    Added CORS support and implement post creation and feed display
    - Added CORS middleware to the backend for handling cross-origin requests.
    - Implemented a new route to fetch posts from the database.
    - Created a new CreatePost component for submitting posts with image and caption.
    - Developed a Feed component to display the list of posts.
    - Updated routing in the frontend to include CreatePost and Feed pages.
    - Added Axios for making HTTP requests to the backend.
    - Enhanced styling for the create post and feed sections.

--- PostApp/Backend/src/app.js
+++ PostApp/Backend/src/app.js
@@ -2,9 +2,11 @@ const express = require("express")
 const postModel = require("./models/post.model")
 const multer = require("multer")
 const uploadFile = require("./services/storage.services")
+const cors = require("cors")
 
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 const upload = multer({storage: multer.memoryStorage()})
 
@@ -23,6 +25,16 @@ app.post("/create-post",upload.single("image"), async (req, res)=>{
         message: "Post created successfully",
         post
 
+    })
+})
+
+app.get("/posts",async (req, res)=>{
+
+    const posts = await postModel.find()
+
+    return res.status(200).json({
+        message: "Posts fetched successfully",
+        posts
     })
 })
 
