5e42bf53ade08d8a60ebc097e518a8df3083a841
Zain Shaikh <skzain007995@gmail.com>
2026-07-25 18:45:06 +0530

    initialized Spotify project with Express and MongoDB setup

---
+++ Spotify/server.js
@@ -0,0 +1,12 @@
+const connectDB = require("./src/db/db")
+const app = require("./src/app")
+require("dotenv").config()
+
+
+connectDB()
+
+
+
+app.listen(3000, ()=>{
+    console.log("Server is running on port 3000")
+})
---
+++ Spotify/src/db/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+const dns = require("dns")
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+
+async function connectDB(){
+
+    try {
+
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Connected to Database successfully")
+
+    } catch (error) {
+        console.log("Database connection error", error)
+    }
+}
+
+
+module.exports = connectDB
