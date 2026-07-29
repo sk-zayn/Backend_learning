971d8721ae431aa6bb02e362c5024da449f60a79
Zain Shaikh <skzain007995@gmail.com>
2026-07-29 17:38:40 +0530

    Implement album creation functionality and enhance music creation with artist authentication

--- Spotify/src/controllers/music.controller.js
+++ Spotify/src/controllers/music.controller.js
@@ -1,51 +1,49 @@
 const musicModel = require("../models/music.model");
+const albumModel = require("../models/album.model");
 const { uploadFile } = require("../services/storage.service");
 const jwt = require("jsonwebtoken");
 // require("dotenv").config();
 
 async function createMusic(req, res) {
-      console.log("HIT createMusic", req.file, req.body);
+  const { title } = req.body;
+  const file = req.file;
 
-  const token = req.cookies.token;
+  const result = await uploadFile(req.file);
 
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
+  const music = await musicModel.create({
+    uri: result.url,
+    title,
+    artist: req.user.id,
+  });
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    if (decoded.role !== "artist") {
-      return res
-        .status(403)
-        .json({ message: "You don't have access to create music" });
-    }
-
-    const { title } = req.body;
-    const file = req.file;
-
-    const result = await uploadFile(req.file);
-
-    const music = await musicModel.create({
-      uri: result.url,
-      title,
-      artist: decoded.id,
-    });
-
-    res.status(201).json({
-      message: "Music created successfully",
-      music: {
-        id: music._id,
-        uri: music.uri,
-        title: music.title,
-        artist: music.artist,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    
-    return res.status(401).json({ message: "Unauthorized" });
-  }
+  res.status(201).json({
+    message: "Music created successfully",
+    music: {
+      id: music._id,
+      uri: music.uri,
+      title: music.title,
+      artist: music.artist,
+    },
+  });
 }
 
-module.exports = { createMusic };
+async function createAlbum(req, res) {
+  const { title, musics } = req.body;
+  const album = await albumModel.create({
+    title,
+    artist: req.user.id,
+    musics: musics,
+  });
+
+  res.status(201).json({
+    message: "Album created successfully",
+    album: {
+      id: album._id,
+      title: album.title,
+      artist: album.artist,
+      musics: album.musics,
+    },
+  });
+}
+
+module.exports = { createMusic, createAlbum };
--- Spotify/src/routes/music.routes.js
+++ Spotify/src/routes/music.routes.js
@@ -1,12 +1,19 @@
 const express = require("express");
-const multer = require("multer")
+const multer = require("multer");
+const authMiddleware = require("../middlewares/auth.middleware");
 const musicController = require("../controllers/music.controller");
 
 const router = express.Router();
-const upload  = multer({
-    storage: multer.memoryStorage()
-})
+const upload = multer({
+  storage: multer.memoryStorage(),
+});
 
-router.post("/upload", upload.single("music"), musicController.createMusic)
+router.post(
+  "/upload",
+  authMiddleware.authArtist,
+  upload.single("music"),
+  musicController.createMusic,
+);
+router.post("/album", authMiddleware.authArtist, musicController.createAlbum);
 
 module.exports = router;
